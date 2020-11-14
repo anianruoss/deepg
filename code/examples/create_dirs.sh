60c3183b19b9802ffdef754bd99aa03ebcc5d264
anianruoss <anian.ruoss@gmail.com>
2020-11-14 03:59:39 +0000

    add experiment file

--- code/examples/create_dirs.sh
+++ code/examples/create_dirs.sh
@@ -1,68 +1,128 @@
-old_dir=modelnet40_64p_rotation_theta_1_intervals_1_eps_0.0000001
+# RotationZ
+old_dir=modelnet40_64p_rotation_z_theta_1_intervals_1_eps_0.0000001
 
-for eps in 0.0001 0.00001 0.000001 0.0000001
+for theta in 2 3 5
 do
-    experiment_dir=rotation_1024p_${eps}
-    mkdir ${experiment_dir}
-
-    for theta in 1 2 3 4 5 15 30 45 60 75 90
-    do
-        new_dir=${experiment_dir}/modelnet40_1024p_rotation_theta_${theta}_intervals_1_eps_${eps}
-        cp -r ${old_dir} ${new_dir}
-        sed -i "s/RotationZ(-1,1);RotationY(-1,1);RotationX(-1,1)/RotationZ(-${theta},${theta});RotationY(-${theta},${theta});RotationX(-${theta},${theta})/" ${new_dir}/config.txt
-        sed -i "s/num_points            64/num_points            1024/" ${new_dir}/config.txt
-        sed -i "s/poly_eps              0.0000001/poly_eps              ${eps}/" ${new_dir}/config.txt
-    done
+    new_dir=modelnet40_64p_rotation_z_theta_${theta}_intervals_1_eps_0.0000001
+    cp -r ${old_dir} ${new_dir}
+    sed -i "s/RotationZ(-1,1)/RotationZ(-${theta},${theta})/" ${new_dir}/config.txt
 done
 
-old_dir=modelnet40_64p_shearing_theta_1_intervals_1_eps_0.0000001
-
-for eps in 0.0001 0.00001 0.000001 0.0000001
+for theta in 10 20 30
 do
-    experiment_dir=shearing_1024p_${eps}
-    mkdir ${experiment_dir}
-
-    for theta in 1 2 3 4 5 15 30 45 60 75 90
-    do
-        new_dir=${experiment_dir}/modelnet40_1024p_shearing_theta_${theta}_intervals_1_eps_${eps}
-        cp -r ${old_dir} ${new_dir}
-        sed -i "s/ShearZ(-1,1,-1,1);ShearY(-1,1,-1,1);ShearX(-1,1,-1,1)/ShearZ(-${theta},${theta},-${theta},${theta});ShearY(-${theta},${theta},-${theta},${theta});ShearX(-${theta},${theta},-${theta},${theta})/" ${new_dir}/config.txt
-        sed -i "s/num_points            64/num_points            1024/" ${new_dir}/config.txt
-        sed -i "s/poly_eps              0.0000001/poly_eps              ${eps}/" ${new_dir}/config.txt
-    done
+    new_dir=modelnet40_64p_rotation_z_theta_${theta}_intervals_${theta}_eps_0.0000001
+    cp -r ${old_dir} ${new_dir}
+    sed -i "s/RotationZ(-1,1)/RotationZ(-${theta},${theta})/" ${new_dir}/config.txt
+    sed -i "s/chunks                1/chunks                ${theta}/" ${new_dir}/config.txt
 done
 
-old_dir=modelnet40_64p_twisting_theta_1_intervals_1_eps_0.0000001
 
-for eps in 0.0001 0.00001 0.000001 0.0000001
+# RotationZ; RotationX
+old_dir=modelnet40_64p_rotation_z_x_theta_1_intervals_1_eps_0.0000001
+
+for theta in 2 3 5
 do
-    experiment_dir=twisting_1024p_${eps}
-    mkdir ${experiment_dir}
-
-    for theta in 1 2 3 4 5 15 30 45 60 75 90
-    do
-        new_dir=${experiment_dir}/modelnet40_1024p_twisting_theta_${theta}_intervals_1_eps_${eps}
-        cp -r ${old_dir} ${new_dir}
-        sed -i "s/TwistingZ(-1,1)/TwistingZ(-${theta},${theta})/" ${new_dir}/config.txt
-        sed -i "s/num_points            64/num_points            1024/" ${new_dir}/config.txt
-        sed -i "s/poly_eps              0.0000001/poly_eps              ${eps}/" ${new_dir}/config.txt
-    done
+    new_dir=modelnet40_64p_rotation_z_x_theta_${theta}_intervals_1_eps_0.0000001
+    cp -r ${old_dir} ${new_dir}
+    sed -i "s/RotationX(-1,1)/RotationX(-${theta},${theta})/" ${new_dir}/config.txt
+    sed -i "s/RotationZ(-1,1)/RotationZ(-${theta},${theta})/" ${new_dir}/config.txt
 done
 
-old_dir=modelnet40_64p_tapering_theta_1_intervals_1_eps_0.0000001
-
-for eps in 0.0001 0.00001 0.000001 0.0000001
+for theta in 2 3 4 5
 do
-    experiment_dir=tapering_1024p_${eps}
-    mkdir ${experiment_dir}
-
-    for theta in 1 2 3 4 5 15 30 45 60 75 90
-    do
-        new_dir=${experiment_dir}/modelnet40_1024p_tapering_theta_${theta}_intervals_1_eps_${eps}
-        cp -r ${old_dir} ${new_dir}
-        sed -i "s/TaperingZ(-1,1,-1,1)/TaperingZ(-${theta},${theta},-${theta},${theta})/" ${new_dir}/config.txt
-        sed -i "s/num_points            64/num_points            1024/" ${new_dir}/config.txt
-        sed -i "s/poly_eps              0.0000001/poly_eps              ${eps}/" ${new_dir}/config.txt
-    done
+    new_dir=modelnet40_64p_rotation_z_x_theta_${theta}_intervals_${theta}_eps_0.0000001
+    cp -r ${old_dir} ${new_dir}
+    sed -i "s/RotationX(-1,1)/RotationX(-${theta},${theta})/" ${new_dir}/config.txt
+    sed -i "s/RotationZ(-1,1)/RotationZ(-${theta},${theta})/" ${new_dir}/config.txt
+    sed -i "s/chunks                1/chunks                ${theta}/" ${new_dir}/config.txt
 done
 
+
+# RotationZ; RotationY; RotationX
+old_dir=modelnet40_64p_rotation_z_y_x_theta_1_intervals_1_eps_0.0000001
+
+for theta in 1.5 2
+do
+    new_dir=modelnet40_64p_rotation_z_y_x_theta_${theta}_intervals_1_eps_0.0000001
+    cp -r ${old_dir} ${new_dir}
+    sed -i "s/RotationX(-1,1)/RotationX(-${theta},${theta})/" ${new_dir}/config.txt
+    sed -i "s/RotationY(-1,1)/RotationY(-${theta},${theta})/" ${new_dir}/config.txt
+    sed -i "s/RotationZ(-1,1)/RotationZ(-${theta},${theta})/" ${new_dir}/config.txt
+done
+
+for theta in 2 3
+do
+    old_dir=modelnet40_64p_rotation_z_y_x_theta_${theta}_intervals_${theta}_eps_0.0000001
+    cp -r ${old_dir} ${new_dir}
+    sed -i "s/RotationX(-1,1)/RotationX(-${theta},${theta})/" ${new_dir}/config.txt
+    sed -i "s/RotationY(-1,1)/RotationY(-${theta},${theta})/" ${new_dir}/config.txt
+    sed -i "s/RotationZ(-1,1)/RotationZ(-${theta},${theta})/" ${new_dir}/config.txt
+    sed -i "s/chunks                1/chunks                ${theta}/" ${new_dir}/config.txt
+done
+
+
+# ShearZ
+old_dir=modelnet40_64p_shearing_z_theta_0.01_intervals_1_eps_0.0000001
+
+for theta in 0.02 0.03
+do
+    new_dir=modelnet40_64p_shearing_z_theta_${theta}_intervals_1_eps_0.0000001
+    cp -r ${old_dir} ${new_dir}
+    sed -i "s/ShearZ(-0.01,0.01,-0.01,0.01)/ShearZ(-${theta},${theta},-${theta},${theta})/" ${new_dir}/config.txt
+done
+
+
+# TaperingZ; RotationZ
+old_dir=modelnet40_64p_tapering_z_rotation_z_theta_0.1_1_intervals_1_eps_0.0000001
+
+for theta in 0.2 0.3
+do
+    new_dir=modelnet40_64p_tapering_z_rotation_z_theta_${theta}_1_intervals_1_eps_0.0000001
+    cp -r ${old_dir} ${new_dir}
+    sed -i "s/TaperingZ(-0.1,0.1,-0.1,0.1)/TaperingZ(-${theta},${theta},-${theta},${theta})/" ${new_dir}/config.txt
+done
+
+
+# TaperingZ
+old_dir=modelnet40_64p_tapering_z_theta_0.1_intervals_1_eps_0.0000001
+
+for theta in 0.2 0.3
+do
+    new_dir=modelnet40_64p_tapering_z_theta_${theta}_intervals_1_eps_0.0000001
+    cp -r ${old_dir} ${new_dir}
+    sed -i "s/TaperingZ(-0.1,0.1,-0.1,0.1)/TaperingZ(-${theta},${theta},-${theta},${theta})/" ${new_dir}/config.txt
+done
+
+
+# TwistingZ; RotationZ
+old_dir=modelnet40_64p_twisting_z_rotation_z_theta_10_1_intervals_1_eps_0.0000001
+
+for theta in 20 30
+do
+    new_dir=modelnet40_64p_twisting_z_rotation_z_theta_${theta}_1_intervals_1_eps_0.0000001
+    cp -r ${old_dir} ${new_dir}
+    sed -i "s/TwistingZ(-10,10)/TwistingZ(-${theta},${theta})/" ${new_dir}/config.txt
+done
+
+
+# TwistingZ; TaperingZ; RotationZ
+old_dir=modelnet40_64p_twisting_z_tapering_z_rotation_z_theta_10_0.1_1_intervals_1_eps_0.0000001
+
+for theta in 2 3
+do
+    new_dir=modelnet40_64p_twisting_z_tapering_z_rotation_z_theta_${theta}0_0.${theta}_1_intervals_1_eps_0.0000001
+    cp -r ${old_dir} ${new_dir}
+    sed -i "s/TwistingZ(-10,10)/TwistingZ(-${theta}0,${theta}0)/" ${new_dir}/config.txt
+    sed -i "s/TaperingZ(-0.1,0.1,-0.1,0.1)/TaperingZ(-0.${theta},0.${theta},-0.${theta},0.${theta})/" ${new_dir}/config.txt
+done
+
+
+# TwistingZ
+old_dir=modelnet40_64p_twisting_z_theta_10_intervals_1_eps_0.0000001
+
+for theta in 20 30
+do
+    new_dir=modelnet40_64p_twisting_z_rotation_z_theta_${theta}_1_intervals_1_eps_0.0000001
+    cp -r ${old_dir} ${new_dir}
+    sed -i "s/TwistingZ(-10,10)/TwistingZ(-${theta},${theta})/" ${new_dir}/config.txt
+done
